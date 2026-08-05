4a060fcee7d7a6cb590e0aac472d93285f433dbe
S. I. Khan <79093080+Swar-i-K@users.noreply.github.com>
2026-08-05 20:36:23 +0600

    Implement DateTime function to update time every second
    Fix syntax errors and improve DateTime function.

---
+++ script.js
@@ -0,0 +1,6 @@
+function DateTime(){
+const dt new Date();
+document.getElementById('dt1").innerTest dt.toLocaleString();
+DateTime();
+  setInterval(DateTimel, 1000);
+  setInterval(f()>dt2. innerText new Date().toLocaleString(),1080), f();
